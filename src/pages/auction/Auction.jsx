c1b05e7fd451e8d168956495e4e83602c0a591b8
kimminjae <96830595+kareawe@users.noreply.github.com>
2024-05-29 18:46:46 +0900

    최종 Auction.jsx

--- src/pages/auction/Auction.jsx
+++ src/pages/auction/Auction.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const Auction = () => {
-  const [maxValue, setMaxValue] = useState(0);
-  const [secondMaxValue, setSecondMaxValue] = useState(0);
+  const [auctionPrices, setAuctionPrices] = useState({});
   const [currentTime, setCurrentTime] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
     const updateTime = () => {
@@ -24,29 +25,77 @@ const Auction = () => {
     updateTime();
     const intervalId = setInterval(updateTime, 1000);
 
-    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
   const saveValue = () => {
     const enteredValue = parseInt(inputValue);
-    if (enteredValue > maxValue) {
-      setSecondMaxValue(maxValue);
-      setMaxValue(enteredValue);
-    } else if (enteredValue > secondMaxValue) {
-      setSecondMaxValue(enteredValue);
+    if (enteredValue % 10000 !== 0) {
+      alert("금액은 만원 단위로 입력해주세요.");
+      return;
     }
+    const key = `${selectedCategory}_${selectedRegion}`;
+    setAuctionPrices((prevState) => {
+      const currentAuction = prevState[key] || {};
+      const { maxValue, secondMaxValue } = currentAuction;
+
+      if (!maxValue || enteredValue > maxValue) {
+        return {
+          ...prevState,
+          [key]: {
+            maxValue: enteredValue,
+            secondMaxValue:
+              maxValue !== undefined
+                ? enteredValue !== maxValue
+                  ? maxValue
+                  : secondMaxValue
+                : secondMaxValue,
+          },
+        };
+      } else if (
+        !secondMaxValue ||
+        (enteredValue > secondMaxValue && enteredValue !== maxValue)
+      ) {
+        return {
+          ...prevState,
+          [key]: {
+            ...currentAuction,
+            secondMaxValue: enteredValue,
+          },
+        };
+      }
+
+      return prevState;
+    });
+
+    setInputValue("");
   };
 
-  const handleKeyDown = (e) => {
+  const handleRegionChange = (e) => {
+    setSelectedRegion(e.target.value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       saveValue();
     }
   };
 
+  const numberWithCommas = (x) => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  const currentKey = `${selectedCategory}_${selectedRegion}`;
+  const currentAuction = auctionPrices[currentKey] || {};
+
   return (
     <div className="relative min-h-screen flex flex-col bg-scary-gradient bg-[length:200%_200%] animate-bg-move text-white overflow-hidden">
       <header className="p-4">
-        <div className="text-center text-3xl font-bold">경매장</div>
+        <div className="text-center text-3xl font-bold">광고 경매장</div>
       </header>
       <main className="flex-grow flex flex-col items-center justify-center relative">
         <div className="text-center mb-4">
@@ -58,32 +107,81 @@ const Auction = () => {
             {currentTime}
           </div>
         </div>
-        <div className="bg-gray-800 p-6 rounded-lg shadow-xl w-80">
-          <div className="mt-0">
-            <p className="mb-2">
-              저장된 최대 값: <span id="maxValue">{maxValue}원</span>
-            </p>
-            <p className="mb-4">
-              저장된 두 번째로 큰 값:{" "}
-              <span id="secondMaxValue">{secondMaxValue}원</span>
-            </p>
+        <div className="bg-gray-800 p-6 rounded-lg shadow-xl w-120">
+          <div className="font-digital-7 text-2xl mt-0">
+            {selectedRegion && selectedCategory && (
+              <>
+                <p className="mb-2">
+                  최고 입찰가:{" "}
+                  <span id="maxValue">
+                    {numberWithCommas(currentAuction.maxValue || 0)}원
+                  </span>
+                </p>
+                <p className="mb-2">
+                  2번째 높은 입찰가 :{" "}
+                  <span id="secondMaxValue">
+                    {numberWithCommas(currentAuction.secondMaxValue || 0)}원
+                  </span>
+                </p>
+              </>
+            )}
           </div>
-          <input
-            type="number"
-            id="bidInput"
-            placeholder="값을 입력하세요"
-            className="w-full p-2 mb-4 rounded-lg bg-gray-700 text-white"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleKeyDown}
-          />
-          <button
-            id="bidbutton"
-            onClick={saveValue}
-            className="w-full p-2 bg-red-700 hover:bg-red-800 rounded-lg text-white"
+          <select
+            value={selectedRegion}
+            onChange={handleRegionChange}
+            className="w-full p-2 mb-2 rounded-lg bg-gray-700 text-white"
           >
-            저장하기
-          </button>
+            <option value="">지역 선택</option>
+            <option value="서울특별시">서울특별시</option>
+            <option value="부산광역시">부산광역시</option>
+            <option value="대구광역시">대구광역시</option>
+            <option value="인천광역시">인천광역시</option>
+            <option value="광주광역시">광주광역시</option>
+            <option value="대전광역시">대전광역시</option>
+            <option value="울산광역시">울산광역시</option>
+            <option value="세종특별자치시">세종특별자치시</option>
+            <option value="경기도">경기도</option>
+            <option value="강원도">강원도</option>
+            <option value="충청북도">충청북도</option>
+            <option value="충청남도">충청남도</option>
+            <option value="전라북도">전라북도</option>
+            <option value="전라남도">전라남도</option>
+            <option value="경상북도">경상북도</option>
+            <option value="경상남도">경상남도</option>
+            <option value="제주특별자치도">제주특별자치도</option>
+          </select>
+          <select
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            className="w-full p-2 mb-2 rounded-lg bg-gray-700 text-white"
+          >
+            <option value="">업종 선택</option>
+            <option value="한식">한식</option>
+            <option value="중식">중식</option>
+            <option value="양식">양식</option>
+            <option value="일식">일식</option>
+            <option value="카페">카페</option>
+          </select>
+          {selectedRegion && selectedCategory && (
+            <>
+              <input
+                type="text"
+                id="bidInput"
+                placeholder="만원 단위 금액을 입력하세요"
+                className="w-full p-2 mb-4 rounded-lg bg-gray-700 text-white"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyPress={handleKeyPress}
+              />
+              <button
+                id="bidbutton"
+                onClick={saveValue}
+                className="w-full p-2 bg-red-700 hover:bg-red-800 rounded-lg text-white"
+              >
+                저장하기
+              </button>
+            </>
+          )}
         </div>
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
           <div className="absolute top-20 left-10 w-10 h-10 bg-white rounded-full opacity-30 animate-ghost-float"></div>
