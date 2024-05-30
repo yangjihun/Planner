f0588b0786c415517112cfbebfde116a0b5cbc3d
Jihun <yjhn0410@gmail.com>
2024-05-30 19:30:51 +0900

    logout 구현

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = ({ name }) => {
+const Header = ({ name, setName }) => {
+  const clk = () => {
+    setName('')
+  }
   return (
     <header className="bg-white text-gray-600 body-font shadow w-full">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center w-full"> {/* w-full 추가 */}
@@ -11,7 +14,7 @@ const Header = ({ name }) => {
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center w-full md:w-auto">
           <Link to="/" className="mr-10 hover:text-headerTextColor font-semibold hover:scale-105 transition duration-150">Home</Link>
           <Link to="/auction" className="mr-10 hover:text-headerTextColor font-semibold hover:scale-105 transition duration-150">Auction</Link>
-          <Link to="/login" className="mr-10 hover:text-headerTextColor font-semibold hover:scale-105 transition duration-150">Login</Link>
+          <Link to="/login" className="mr-10 hover:text-headerTextColor font-semibold hover:scale-105 transition duration-150">{name==='' ? 'Login' : <div onClick={()=>clk()}>Logout</div>}</Link>
         </nav>
       </div>
     </header>
